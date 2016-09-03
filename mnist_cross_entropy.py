e6c6711aad55b046b14b4c0229c84ce5c6561610
Ryan Dao <ddao@paypal.com>
2016-09-03 17:53:55 +0800

    Save model

--- mnist_cross_entropy.py
+++ mnist_cross_entropy.py
@@ -1,10 +1,9 @@
-from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Import data
-from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import json
+from tensorflow.examples.tutorials.mnist import input_data
 
 class MNISTNetwork:
     def __init__(self, sizes):
@@ -28,11 +27,12 @@ class MNISTNetwork:
         logit = self.logit(inputs)
         cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logit, labels))
         train_step = tf.train.GradientDescentOptimizer(3.0).minimize(cost)
-        mnist = input_data.read_data_sets('/tmp/data', one_hot=True)
+        mnist = input_data.read_data_sets('data', one_hot=True)
+        saver = tf.train.Saver()
 
         with tf.Session() as session:
             session.run(tf.initialize_all_variables())
-            for i in range(100):
+            for i in range(10):
                 while not mnist.train.epochs_completed:
                     batch_xs, batch_ys = mnist.train.next_batch(10)
                     session.run(train_step, {inputs: batch_xs, labels: batch_ys})
@@ -42,7 +42,10 @@ class MNISTNetwork:
                 # Test trained model
                 correct_prediction = tf.equal(tf.argmax(logit, 1), tf.argmax(labels, 1))
                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-                print(accuracy.eval({inputs: mnist.test.images, labels: mnist.test.labels}))
+                print("Epoch %s:" % i, accuracy.eval({inputs: mnist.test.images, labels: mnist.test.labels}))
+
+            save_path = saver.save(session, "model.ckpt")
+            print("Model saved to %s" % save_path)
 
 network = MNISTNetwork([784, 30, 10])
 network.train()
