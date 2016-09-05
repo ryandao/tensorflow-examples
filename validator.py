433049d102c1361c74fe073e2487b3d5e570b37f
Ryan Dao <ddao@paypal.com>
2016-09-05 08:11:09 +0800

    Validation script

---
+++ validator.py
@@ -0,0 +1,16 @@
+from __future__ import division
+from __future__ import print_function
+
+import sys
+from mnist_network import MNISTNetwork
+import tensorflow as tf
+from PIL import Image
+
+path = sys.argv[1]
+img = Image.open(path).convert('LA')
+data = [x[1] / 255 for x in img.getdata()]
+
+with tf.Session() as session:
+    network = MNISTNetwork([784, 30, 10])
+    network.load(session, 'model.ckpt')
+    print("Network prediction: %s" % network.feedforward(session, [data])[0])
